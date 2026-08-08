13be9540d5af71a49677b5f6e169ffcb2f556071
sourabh <sourabhlachheta@gmail.com>
2026-08-08 22:41:00 +0530

    creating inventory

---
+++ Lord-Of-Realms/src/inventory/Inventory.java
@@ -0,0 +1,14 @@
+package inventory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Inventory {
+    private List<Item> items;
+
+    public Inventory(){
+        items = new ArrayList<>();
+    }
+}
